9b86d84ab6ca5e3b2f9847be63d1c96972760aa8
Pingwinnek <pingwinkrol1@gmail.com>
2021-03-04 19:09:15 +0100

    With basic Dziennik and Student class implementation (to bo continued)

---
+++ main.py
@@ -0,0 +1,6 @@
+from dziennik import Dziennik
+
+d = Dziennik(rok= 2021, litera= "c")
+d.dodaj_studenta(imie="Jan", nazwisko="Nowak")
+d.dodaj_studenta(imie="Jan", nazwisko="Kowalski")
+print(d.podaj_ilosc_studentow())
